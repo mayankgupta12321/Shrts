ca3d2d41a0f2df72d7565045f66bc61b3b199354
Mayank Gupta <mayankgupta12321@gmail.com>
2020-07-26 13:35:34 +0530

    Second Commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'mongoUrl': 'mongodb+srv://dbUser:dbUser@shrts-kbu6c.mongodb.net/shrts?retryWrites=true&w=majority',
-    'baseUrl': 'http://192.168.43.47:3000/'
+    'baseUrl': 'https://shrts.herokuapp.com/'
 }
